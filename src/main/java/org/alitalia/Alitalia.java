23e3bc2a1039524988e264c4db3e2b08810e05b7
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-07-09 16:04:20 +0530

    Flight searching code added by saranya

--- src/main/java/org/alitalia/Alitalia.java
+++ src/main/java/org/alitalia/Alitalia.java
@@ -1,18 +1,29 @@
 package org.alitalia;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Alitalia {
 	
 	public static void main(String[] args) {
 	
-System.setProperty("webdriver.chrome.driver", "E:\\Selenium Project\\WorkSpace\\LoginFb\\Driver\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\SUTHAKAR\\eclipse-workspace\\SecondProgram\\driver\\chromedriver.exe");
 		
 		WebDriver driver= new ChromeDriver();
 		
 		driver.get("https://www.alitalia.com/en_en/homepage.html");
-		
+		driver.manage().window().maximize();
+		//Searching flight
+		WebElement srchClick = driver.findElement(By.id("main_search"));
+		srchClick.click();
+		WebElement search = driver.findElement(By.id("search-bar__input"));
+		search.sendKeys("Chennai to Delhi");
+		WebElement ok = driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
+		ok.click();
 	}
 
 }

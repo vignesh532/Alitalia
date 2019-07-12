535158f7bfeb3808e90463bbdebda186d584cdeb
ManikhandanS <balakarthi13@gmail.com>
2019-07-12 09:51:25 +0530

    New Code Added

---
+++ src/Test/A.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class A {
+	
+		 static int id=200;
+		 protected static void empid() 
+		 {
+			 System.out.println("Emp Id is:"+id);
+		 }
+		 public static void empname() 
+		 {
+			 System.out.println("Empname is:"+id);
+	     }
+		 public static void main(String[] args)
+		 {
+			empid();
+			System.out.println(id);
+		 }
+	 
+}
+
---
+++ src/Test/B.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class B extends A 
+{
+public static void main(String[] args)
+{
+	A.empid();
+	A.empname();
+}
+}
